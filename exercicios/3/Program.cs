72605b5cdbf725765551f6b780d24b5273d61b8a
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-03-29 17:26:19 -0300

    exercicio

---
+++ exercicios/3/Program.cs
@@ -0,0 +1,24 @@
+﻿//faca um programa q tire a media aritimetica de um aluno com 5 notas
+
+Console.WriteLine($"Digite seu nome");
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Olá querida (o), {nome}. Digite sua n1");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Olá querido, {nome}. Digite sua n2");
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Olá querido, {nome}. Digite sua n3");
+int n3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Olá querido, {nome}. Digite sua n4");
+int n4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Olá querido, {nome}. Digite sua n5");
+int n5 = int.Parse(Console.ReadLine());
+
+int media = (n1 + n2 + n3 + n4 + n5) / 5;
+
+Console.WriteLine($"Parabens! Sua média é {media}");
+
