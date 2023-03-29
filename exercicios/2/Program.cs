b71b4e6bf3a22d293c5841296f96a5894e61b701
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-03-29 16:52:57 -0300

    exercicio

---
+++ exercicios/2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Faça um programa que receba o ano do nascimento de uma pessoa e calcule a idade dessa pessoa em anos
+//e semanas e imprima o resultado no console.
+
+Console.WriteLine($"Digite seu ano de nascimento");
+int ano = int.Parse(Console.ReadLine());
+
+int idade = DateTime.Now.Year - ano;
+int semana = idade*52;
+
+Console.WriteLine($"sua idade é: {idade}");
+Console.WriteLine($"sua idade em semanas é: {semana}");
+
+
