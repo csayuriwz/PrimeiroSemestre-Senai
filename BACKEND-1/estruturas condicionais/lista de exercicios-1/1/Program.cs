6f3859fe0cd6f41d5433148aae4b26556ea5848f
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-03-30 16:54:01 -0300

    exercicio 1

---
+++ BACKEND-1/estruturas condicionais/lista de exercicios-1/1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Faça um programa que o usuário informe o salário recebido e o total gasto. Deverá ser exibido na tela “Gastos dentro do orçamento” caso o valor gasto não ultrapasse o valor do salário e “Orçamento estourado” se o valor gasto ultrapassar o valor do salário.
+
+Console.WriteLine($"Qual o seu sálario liquido?");
+float salário = float.Parse (Console.ReadLine());
+
+Console.WriteLine($"Quanto você gastou esse mês?");
+float gasto = float.Parse (Console.ReadLine());
+
+if (gasto > salário)
+{
+    Console.WriteLine($"Ops! Seu orcamento desse mes foi estourado :( ");
+}
+
+else if (gasto < salário);
+{
+    Console.WriteLine($"Os gastos desse mês estão dentro do orcamento! Parabens!");
+}
