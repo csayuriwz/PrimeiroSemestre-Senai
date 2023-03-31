3e68dea8e718285466a94f0e706c79f41b77592f
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-03-31 14:38:07 -0300

    exercicio 6

---
+++ BACKEND-1/estruturas condicionais/lista de exercicios-1/4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Escreva     um     programa     que     verifique     a     validade     de     uma     senha     fornecida pelo     usuário. 
+//A     senha     válida     é     o     número     1234. 
+//Devem     ser    impressas     as    seguintes     mensagens:
+//ACESSO    PERMITIDO    caso    a    senha    seja    válida.
+//ACESSO    NEGADO    caso    a    senha    seja    inválida.
+
+Console.WriteLine($"Digite sua senha?");
+int senha = int.Parse(Console.ReadLine());
+
+if (senha == 1234)
+{
+    Console.WriteLine($"ACESSO PERMITIDO ");
+}
+
+else
+{
+    Console.WriteLine($"ACESSO NEGADO");
+}
+
+
