7c347779803d81cc08a7bb86c0b8ea121a9cce31
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-04-03 14:30:26 -0300

    exercicos

--- BACKEND-1/estruturas condicionais/lista de exercicios-1/2/Program.cs
+++ BACKEND-1/estruturas condicionais/lista de exercicios-1/2/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿// Desenvolva um programa que recebe do usuário, o placar de um jogo de futebol (os gols de cada time) e informa se o resultado foi um empate, se a vitória foi do primeiro time ou do segundo time.
+
+Console.WriteLine($"Informe os gols do time 1: ");
+int time1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe os gols do time 2: ");
+int time2 = int.Parse(Console.ReadLine());
+
+if( time1 > time2)
+{
+    Console.WriteLine($"Vitoria do time 1 placar de {time1} x {time2}");
+}
+
+else if( time1 < time2)
+{
+    Console.WriteLine($"Vitoria do time 2 placar de {time2} x {time1}");
+}
+
+else
+{
+    Console.WriteLine($"EMPATE!");
+}
---
+++ BACKEND-1/estruturas condicionais/lista de exercicios-1/3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Escreva um programa que leia as medidas dos lados de um triângulo e escreva se ele é Equilátero, Isósceles ou Escaleno. 
+// Sendo que:    
+// − Triângulo Equilátero: possui os 3 lados iguais.
+// − Triângulo Isóscele: possui 2 lados iguais.
+// − Triângulo Escaleno: possui 3 lados diferentes.
+
+Console.WriteLine($"Qual e o tamanho de um dos lados do triangulo? ");
+int lado1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Qual e o tamanho de um dos lados do triangulo? ");
+int lado2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Qual e o tamanho de um dos lados do triangulo? ");
+int lado3 = int.Parse(Console.ReadLine());
+
+if (lado1 == lado2  && lado1 == lado3)
+{
+    Console.WriteLine($"Triângulo Equilátero: possui os 3 lados iguais.");
+}
+
+else if (lado1 != lado2  && lado1 != lado3)
+{
+    Console.WriteLine($"Triângulo Escaleno: possui 3 lados diferentes.");
+}
+
+else 
+{
+    Console.WriteLine($"Triângulo Isóscele: possui 2 lados iguais.");
+}
