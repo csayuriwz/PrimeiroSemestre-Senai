f51a95ab438e84ebc92a8f75a361505d9190731e
Catarina Sayuri <catarina.arashiro@gmail.com>
2023-04-09 22:31:52 -0300

    exercicio

--- BACKEND-1/estruturas de repeticao/exercicios-desafio/lista/1/Program.cs
+++ BACKEND-1/estruturas de repeticao/exercicios-desafio/lista/1/Program.cs
@@ -1,11 +1,11 @@
 ﻿//1. Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
 
 Console.WriteLine($"Qual sua nota?");
-int idade = int.Parse (Console.ReadLine()!);
+int nota = int.Parse (Console.ReadLine()!);
 
-while (idade < 0 || idade > 10)
+while (nota < 0 || nota > 10)
 {
-    Console.WriteLine($"Nota negada! Digite uma idade valida.");
+    Console.WriteLine($"Nota negada! Digite uma nota valida.");
     idade = int.Parse(Console.ReadLine()!);
     
 }
--- BACKEND-1/estruturas de repeticao/exercicios-desafio/lista/4/Program.cs
+++ BACKEND-1/estruturas de repeticao/exercicios-desafio/lista/4/Program.cs
@@ -8,42 +8,63 @@
 
 
 int sim = 0;
-
 int nao = 0;
-
-//mulheres q responderam sim 
-int msim = 0;
-
-int hnao = 0;
+int mResposta = 0;
+int hResposta = 0;
+int masculino = 0;
 
 
-string sexo = "";
-string opn = "";
 
-for (int i = 1; i <= 10; i++)
-{
-     Console.WriteLine($"Digite seu sexo:");
-      sexo = Console.ReadLine()!;
 
-Console.WriteLine($"Você gostou do produto? responda com sim ou não:");
-     opn = Console.ReadLine()!;
+for (int quantidade = 0; quantidade<10 ;quantidade++){
+
+    Console.WriteLine($"Voce é do sexo masculino ou feminino? m/f");
+    char sexo = char.Parse(Console.ReadLine());
+    if(sexo != 'm' && sexo !='f'){
+        Console.WriteLine($"Invalida, tente outra:");
+        sexo = char.Parse(Console.ReadLine());
+        
+    }
+    else if(sexo == 'm'){
+        masculino++;
+    }
+    
 
     
-     if (opn == "sim")
-     {
-        sim ++;        
-     }
-     else if (true)
-     {
+
+    Console.WriteLine($"Voce gostou do produto? s/n");
+    char resposta = char.Parse(Console.ReadLine());
+    if(resposta != 's' && resposta !='n'){
+        Console.WriteLine($"Invalida, tente outra:");
+        resposta = char.Parse(Console.ReadLine());
         
-     }
-     else
-     {
-        nao ++;
-     }
+    }
+    else if(resposta == 's'){
+        sim++;
+    }
+    else if (resposta == 'n'){
+        nao++;
+    }
 
 
+
+
+   if((sexo == 'f') || (resposta == 's'))
+   {
+    mResposta++;
+   }
+   else if ((sexo) == 'm' || (resposta == 'n')){
+    hResposta++;
+   }
+    
+
+    
 }
 
-Console.WriteLine($"Pessoas que responderam SIM: {sim} pessoas");
+float pHomem;
+pHomem = (hResposta / masculino)*100;
 
+Console.WriteLine($"{sim} responderam que gostaram");
+Console.WriteLine($"{nao} responderam nao gostaram");
+Console.WriteLine($"{mResposta} numero de mulheres que responderam que gostaram");
+Console.WriteLine($"{pHomem}% dos homens votam quem nao gostaram");
