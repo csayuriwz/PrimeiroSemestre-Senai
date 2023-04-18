85089f803633eabd4ecba0b21d4fc721e29bdc3c
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-04-18 17:21:43 -0300

    passagensAereas

--- BACKEND-1/Projeto-passagens-aéreas/Program.cs
+++ BACKEND-1/Projeto-passagens-aéreas/Program.cs
@@ -19,12 +19,12 @@
 
 static void Login()
 {
-    Console.WriteLine($"Olá! Digite sua senha para ter acesso ao menu da nossa agência:");
+    Console.WriteLine($"Olá! Digite sua senha numérica para ter acesso ao menu da nossa agência:");
     int senha = int.Parse(Console.ReadLine()!);
 
     while (senha != 123456)
     {
-        Console.WriteLine($"Acesso negado, tente outra:");
+        Console.WriteLine($"Acesso negado, tente outra vez:");
         senha = int.Parse(Console.ReadLine()!);
     }
     Console.Clear();
@@ -33,71 +33,94 @@ static void Login()
 
 {
     Login();
-    menu();
 }
 
+string[] nome = new string[5];
+string[] origem = new string[5];
+string[] destino = new string[5];
+string[] data = new string[5];
 
 
-
-static void menu()
+int escolha;
+do
 {
-    Console.WriteLine(@$"\nEscolha a opção desejada:
-    1- Cadastrar passagem
-    2- Listar Passagens
-    0- Sair
+    Console.WriteLine(
+
+@$"Escolha a opção desejada:   
+
+    //////////////////////////////////
+    / 1- Cadastrar passagem          /
+    / 2- Listar Passagens            / 
+    / 0- Sair                        /
+    / ////////////////////////////////                             
+
 ");
-    int escolha = int.Parse(Console.ReadLine()!);
-    string[] nome = new string[5];
-    string[] origem = new string[5];
-    string[] destino = new string[5];
-    string[] data = new string[5];
+    escolha = int.Parse(Console.ReadLine()!);
+
     switch (escolha)
     {
         case 1:
-            for (int i = 0; i <= 5; i++)
+
+            char opcao;
+
+            do
             {
-                Console.WriteLine($"Digite seu nome:");
-                nome[i] = Console.ReadLine()!;
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Digite seu nome:");
+                    nome[i] = Console.ReadLine()!;
 
-                Console.WriteLine($"Digite seu local de origem:");
-                origem[i] = Console.ReadLine()!;
+                    Console.WriteLine($"Digite seu local de origem:");
+                    origem[i] = Console.ReadLine()!;
 
-                Console.WriteLine($"Digite seu local de destino:");
-                destino[i] = Console.ReadLine()!;
+                    Console.WriteLine($"Digite seu local de destino:");
+                    destino[i] = Console.ReadLine()!;
 
-                Console.WriteLine($"Digite a data atual:");
-                data[i] = Console.ReadLine()!;
-                Console.Clear();
+                    Console.WriteLine($"Digite a data da viagem:");
+                    data[i] = Console.ReadLine()!;
+                    Console.Clear();
 
+                }
                 Console.WriteLine($"Voce deseja cadastrar uma nova passagem? s/n");
-                char opcao = char.Parse(Console.ReadLine()!);
+                opcao = char.Parse(Console.ReadLine()!);
 
-                while (opcao != 'n' && opcao != 's')
-                {
-                    Console.WriteLine($"Opção invalida, tente outra:");
-                    opcao = char.Parse(Console.ReadLine()!);
 
-                }
 
-                if (opcao == 'n')
-                {
-                    menu();
-                }
-            }
+            } while (opcao == 's');
+
 
             break;
 
         case 2:
 
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine(@$"
+           //////////////////////////////////////////
+           /  Passagem {i + 1}                            /
+           /                                        /
+           /  Nome: {nome[i]},                             /
+           /                                        /
+           /  Local de origem: {origem[i]},                  /
+           /                                        /
+           /  Local de destino: {destino[i]},                 /
+           /                                        /  
+           /  Data da viagem: {data[i]}                    /
+           /                                        /
+           ////////////////////////////////////////// ");
+            }
             break;
         case 0:
-
+            Console.WriteLine($"Fim! Cadastros finalizados.");
             break;
         default:
-
+            Console.WriteLine($"Opcão inválida");
             break;
     }
-}
+} while (escolha != 0);
+
+
 
 
 
