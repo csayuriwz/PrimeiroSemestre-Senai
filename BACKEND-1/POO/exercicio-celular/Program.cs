76b556ffede7dd23a00511a50d78d65359ed46e2
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-04-26 17:15:25 -0300

    exercicio poo

--- BACKEND-1/POO/exercicio-celular/Program.cs
+++ BACKEND-1/POO/exercicio-celular/Program.cs
@@ -2,3 +2,91 @@
 // Com os métodos, ligar, desligar, fazer ligação, enviar mensagem.
 // Só será possível executar tais métodos se o celular estiver ligado.
 // Envie o link do repositório como entrega desta atividade.
+
+using exercicio_celular;
+
+celular celular = new celular();
+
+String escolha;
+
+do
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine(@$"
+=========================================
+Selecione uma das seguingte opcoes:
+[1] - Ligar o aparelho
+[2] - Desligar o aparelho
+[3] - Fazer uma ligacao
+[4] - Enviar mensagens
+[0] - Sair
+==========================================
+");
+    escolha = Console.ReadLine()!;
+
+    switch (escolha)
+    {
+        case "1":
+            {
+                if (celular.Ligado)
+                {
+                    Console.WriteLine($"O celular já está ligado!");
+                }
+                else
+                {
+                    celular.Ligar();
+
+                }
+                break;
+
+            }
+
+        case "2":
+            {
+                if (celular.Ligado == false)
+                {
+                    Console.WriteLine($"O celular já está desligado!");
+                }
+                else
+                {
+                    celular.Desligar();
+
+                }
+                break;
+            }
+        case "3":
+            {
+                if (celular.Ligado)
+                {
+                    celular.FazerLigacao();
+                }
+                else
+                {
+                    Console.WriteLine($"O celular já está desligado!");
+                }
+                break;
+            }
+        case "4":
+            {
+                if (celular.Ligado)
+                {
+                    celular.EnviarMensagem();
+                }
+                else
+                {
+                    Console.WriteLine($"O celular já está desligado!");
+                }
+                break;
+            }
+
+        case "0":
+            {
+                break;
+            }
+
+        default:
+            Console.WriteLine($"Entrada inválida. Tente novamente");
+            break;
+    }
+} while (escolha != "0");
+
--- BACKEND-1/POO/exercicio-celular/celular.cs
+++ BACKEND-1/POO/exercicio-celular/celular.cs
@@ -5,32 +5,40 @@ namespace exercicio_celular
         // definir os atributos
 
         public string Cor = "";
-        public string Modelo = "" ;
+        public string Modelo = "";
         public string Tamanho = "";
-        public bool Ligado ;
+        public bool Ligado;
 
         // definir os metodos
 
-        
+        public void Ligar()
+        {
+            Ligado = true;
+            Console.WriteLine($"Ligando...");
 
+        }
+        public void Desligar()
+        {
+            Ligado = false;
+            Console.WriteLine($"Desligando...");
 
+        }
+        public void FazerLigacao()
+        {
+            Console.WriteLine($"Para quem você quer ligar");
+            string contato = Console.ReadLine()!;
 
+            Console.WriteLine($" Vc está ligando para {contato}");
 
+        }
+        public void EnviarMensagem()
+        {
+            Console.WriteLine($"Pra quem você deseja enviar uma mensagem?");
+            string contato = Console.ReadLine()!;
 
+            Console.WriteLine($"Você esta enviando essa mensagem para {contato}!");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
     }
 }
