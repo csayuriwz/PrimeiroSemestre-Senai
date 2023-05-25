263813b25ca1be88fe219875b022906c3b996b46
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-05-25 17:16:48 -0300

    exerciciomvc

---
+++ BACKEND-2/exercicio-fixacao/Program.cs
@@ -0,0 +1,7 @@
+﻿// Criar um programa console Para cadastro de Eventos. Aplique os conceitos MVC aprendidos em sala de aula.
+
+// O evento deve ter Nome, Descrição e Data que o evento ocorrerá.
+
+// As funcionalidades devem ser Cadastrar e Listar.
+
+
